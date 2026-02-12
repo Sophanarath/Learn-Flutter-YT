498df3da4a55ea70600ac0ee097ad806cb0c0496
Sophanarath <www.sophanarath77@gmail.com>
2026-02-12 22:33:39 +0700

    pain

--- lib/main.dart
+++ lib/main.dart
@@ -17,80 +17,66 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.tealAccent,
       ),
       body: Container(
-        padding: EdgeInsets.all(20),
-        margin: EdgeInsets.all(20),
-        color: Colors.grey[200],
-        child: Container
+        padding: EdgeInsets.all(10),
+        color: Colors.grey[100],
+        child: Row
           (
-          color: Colors.red,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                color: Colors.blueAccent,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Row(
-                      children: [
-                        IconButton(onPressed: () {  }, color: Colors.amberAccent, icon: Icon(Icons.home)),
-                        IconButton(onPressed: () {  }, icon: Icon(Icons.account_balance)),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        IconButton(onPressed: () {  }, icon: Icon(Icons.thumbs_up_down)),
-                        IconButton(onPressed: () {  }, icon: Icon(Icons.add_ic_call_sharp)),
-                      ],
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              flex: 3,
+              child: Image.asset('assets/bg-1.jpg'),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                color: Colors.red,
+                child: Text(
+                    '1',
+                    textAlign: TextAlign.center,
+                    style: TextStyle
+                      (
+                        fontSize: 24,
+                        color: Colors.white,
                     )
-                  ],
                 ),
               ),
-              Container(
-                margin: EdgeInsets.all(40),
-                color: Colors.greenAccent,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Row(
-                      children: [
-                        IconButton(onPressed: () {  }, icon: Icon(Icons.home)),
-                        IconButton(onPressed: () {  }, icon: Icon(Icons.account_balance)),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        IconButton(onPressed: () {  }, icon: Icon(Icons.thumbs_up_down)),
-                        IconButton(onPressed: () {  }, icon: Icon(Icons.add_ic_call_sharp)),
-                      ],
-                    ),
-                  ],
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                color: Colors.orange,
+                child: Text(
+                    '2',
+                    textAlign: TextAlign.center,
+                    style: TextStyle
+                      (
+                      fontSize: 24,
+                      color: Colors.white,
+                    )
                 ),
               ),
-              Container(
-                color: Colors.amberAccent,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Row(
-                      children: [
-                        IconButton(onPressed: () {  }, icon: Icon(Icons.home)),
-                        IconButton(onPressed: () {  }, icon: Icon(Icons.account_balance)),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        IconButton(onPressed: () {  }, icon: Icon(Icons.thumbs_up_down)),
-                        IconButton(onPressed: () {  }, icon: Icon(Icons.add_ic_call_sharp)),
-                      ],
-                    ),
-                  ],
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                color: Colors.yellow,
+                child: Text(
+                    '3',
+                    textAlign: TextAlign.center,
+                    style: TextStyle
+                      (
+                      fontSize: 24,
+                      color: Colors.white,
+                    )
                 ),
               ),
-            ],
-          ),
-        )
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
