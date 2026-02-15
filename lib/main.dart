106c7fefd8cc6f3c7a03e20a5e8f4fbf12a38329
Sophanarath <www.sophanarath77@gmail.com>
2026-02-15 22:09:54 +0700

    #18

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
-
+import 'pet.dart';
 void main() {
   runApp(MaterialApp(
     home: Home(),
   ));
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class Home extends StatefulWidget {
+
+  Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<Pet> pets=[
+    Pet(animal: 'Dog', name: 'Buddha'),
+    Pet(animal: 'Cat', name: 'Jesus'),
+    Pet(animal: 'Bird', name: 'Judas'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,72 +29,10 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.tealAccent,
       ),
-      body: Container(
-        padding: EdgeInsets.all(10),
-        color: Colors.grey[100],
-        child: Row
-          (
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Expanded(
-              flex: 3,
-              child: Image.asset('assets/bg-1.jpg'),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                color: Colors.red,
-                child: Text(
-                    '1',
-                    textAlign: TextAlign.center,
-                    style: TextStyle
-                      (
-                        fontSize: 24,
-                        color: Colors.white,
-                    )
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                color: Colors.orange,
-                child: Text(
-                    '2',
-                    textAlign: TextAlign.center,
-                    style: TextStyle
-                      (
-                      fontSize: 24,
-                      color: Colors.white,
-                    )
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                color: Colors.yellow,
-                child: Text(
-                    '3',
-                    textAlign: TextAlign.center,
-                    style: TextStyle
-                      (
-                      fontSize: 24,
-                      color: Colors.white,
-                    )
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        backgroundColor: Colors.tealAccent,
-        child: Text('Click'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+          children: pets.map((pet) => Text('${pet.animal} :'
+              ' ${pet.name}')).toList(),
       ),
     );
   }
---
+++ lib/pet.dart
@@ -0,0 +1,5 @@
+class Pet {
+  String animal;
+  String name;
+  Pet({required this.animal, required this.name});
+}
